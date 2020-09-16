53e2a4013056ac807e7c221c0b53d25e15ea809b
Fabian Schmid <fs@studer-raimann.ch>
2020-09-16 14:04:50 +0200

    supported versions

--- plugin.php
+++ plugin.php
@@ -1,27 +1,26 @@
 <?php
 /**
  * Copyright (c) 2018 Institut fuer Lern-Innovation, Friedrich-Alexander-Universitaet Erlangen-Nuernberg
- * GPLv2, see LICENSE 
+ * GPLv2, see LICENSE
  */
- 
+
 /**
  * Flashcards plugin: base parameters
  *
  * @author Fred Neumann <fred.neumann@fau.de>
  */
- 
+
 // alphanumerical ID of the plugin; never change this
 $id = "xflc";
- 
+
 // code version; must be changed for all code changes
 $version = "1.5.1";
- 
+
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
-$ilias_min_version = "5.4.0";
-$ilias_max_version = "5.4.999";
- 
+$ilias_min_version = "6.0";
+$ilias_max_version = "6.999";
+
 // optional, but useful: Add one or more responsible persons and a contact email
 $responsible = "Fred Neumann";
 $responsible_mail = "fred.neumann@fau.de";
-?>

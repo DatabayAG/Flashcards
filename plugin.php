902fc26bd44a1edec7160441e62ee5dbe00e87d7
Fabian Schmid <fs@studer-raimann.ch>
2020-09-24 12:07:21 +0200

    support 5.4/6

--- plugin.php
+++ plugin.php
@@ -18,9 +18,10 @@ $version = "1.5.1";
 
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
-$ilias_min_version = "6.0";
+$ilias_min_version = "5.4.0";
 $ilias_max_version = "6.999";
 
 // optional, but useful: Add one or more responsible persons and a contact email
 $responsible = "Fred Neumann";
 $responsible_mail = "fred.neumann@fau.de";
+
